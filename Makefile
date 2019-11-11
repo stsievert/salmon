00d611ea491f722cec1d7acf1548cd7d3036b0c9
Scott Sievert <dev@stsievert.com>
2019-11-11 12:21:11 -0600

    Add docker-compose (and rename)

--- Makefile
+++ Makefile
@@ -1,11 +1,4 @@
-build:
-	docker image build -t salmon:1.0 .
-
-run:
-	docker container run -p 8000:8000 --name sal salmon:1.0
-
-rm:
-	docker container rm --force sal
-
-debug:
-	docker exec -it sal /bin/bash
+debug-loop:
+	docker-compose stop
+	docker-compose build
+	docker-compose up
--- frontend/frontend/frontend.py
+++ frontend/frontend/frontend.py
@@ -6,7 +6,6 @@ from copy import copy
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from jinja2 import Template
-from pathlib import Path
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
@@ -14,30 +13,37 @@ from starlette.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel
 import ujson
 
+from rejson import Client, Path
+
 import numpy as np
 import pandas as pd
 
 from .utils import ServerException
 
+root = Path.rootPath()
+rj = Client(host="redis", port=6379, decode_responses=True)
+rj.jsonset("responses", root, [])
+rj.jsonset("start_time", root, time())
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="templates"), name="static")
-
-responses: List[Dict[str, Any]] = []
-start = time()
 templates = Jinja2Templates(directory="templates")
-exp_data: Dict = {
-    "instructions": "Default instructions (can include <i>arbitrary</i> HTML)"
-}
+
+
+@lru_cache()
+def _get_config():
+    return rj.jsonget("exp_config")
 
 
 async def _ensure_initialized():
-    if not exp_data:
+    if "exp_config" not in rj:
         raise ServerException("No data has been uploaded")
+    exp_config = _get_config()
     expected_keys = ["targets", "instructions", "n"]
-    if not set(exp_data) == set(expected_keys):
+    if not set(exp_config) == set(expected_keys):
         msg = "Experiment keys are not correct. Expected {}, got {}"
-        raise ServerException(msg.format(expected_keys, list(exp_data.keys())))
-    return exp_data
+        raise ServerException(msg.format(expected_keys, list(exp_config.keys())))
+    return exp_config
 
 
 @app.get("/init_exp")
@@ -82,20 +88,23 @@ def upload():
 
 @app.post("/init_file")
 async def init_file(exp: UploadFile = File(default="")):
-    global exp_data
     config = yaml.load(await exp.read(), Loader=yaml.SafeLoader)
-    exp_data.update(config)
-    exp_data["n"] = len(exp_data["targets"])
+    exp_config: Dict = {
+        "instructions": "Default instructions (can include <i>arbitrary</i> HTML)"
+    }
+    exp_config.update(config)
+    exp_config["n"] = len(exp_config["targets"])
+    rj.jsonset("exp_config", root, exp_config)
     return {"success": True}
 
 
 @app.get("/")
 async def get_query_page(request: Request):
-    exp_data = await _ensure_initialized()
+    exp_config = await _ensure_initialized()
     items = {
         "puid": np.random.randint(2 ** 20, 2 ** 32 - 1),
-        "instructions": exp_data["instructions"],
-        "targets": exp_data["targets"],
+        "instructions": exp_config["instructions"],
+        "targets": exp_config["targets"],
     }
     items.update(request=request)
     return templates.TemplateResponse("query_page.html", items)
@@ -103,8 +112,8 @@ async def get_query_page(request: Request):
 
 @app.get("/get_query")
 async def get_query() -> Dict[str, int]:
-    await _ensure_initialized()
-    n = exp_data["n"]
+    exp_config = await _ensure_initialized()
+    n = exp_config["n"]
     h, l, r = list(np.random.choice(n, size=3, replace=False))
     return {"head": int(h), "left": int(l), "right": int(r)}
 
@@ -120,25 +129,27 @@ class Answer(BaseModel):
 @app.post("/process_answer")
 def process_answer(ans: Answer):
     d = ujson.loads(ans.json())
-    d.update({"time_received": time() - start})
-    responses.append(d)
+    d.update({"time_received": time()})
+    rj.jsonarrappend("responses", root, d)
     return {"success": True}
 
 
 @app.get("/get_responses")
 async def get_responses() -> Dict[str, Any]:
-    exp_data = await _ensure_initialized()
-    targets = exp_data["targets"]
-    data = copy(responses)
+    exp_config = await _ensure_initialized()
+    responses = rj.jsonget("responses")
+    targets = exp_config["targets"]
     out: List[Dict[str, Any]] = []
-    for datum in data:
-        datum.update(
+    start = rj.jsonget("start_time")
+    for datum in responses:
+        out.append(datum)
+        out[-1].update(
             {
                 key + "_object": targets[datum[key]]
                 for key in ["left", "right", "head", "winner"]
             }
         )
-        out.append(datum)
+        out[-1].update({"time_received_since_start": datum["time_received"] - start})
 
     return JSONResponse(
         out, headers={"Content-Disposition": 'attachment; filename="responses.json"'}
