e1c9d55e4ff8c7202eb5d2958e6374191b26eba1
Scott <stsievert@users.noreply.github.com>
2020-04-13 15:18:05 -0500

    TST: no repeats in targets

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -71,7 +71,7 @@ def test_basic():
     exp_config = yaml.safe_load(exp.read_bytes())
     puid = "puid-foo"
     answers = []
-    for k in range(20):
+    for k in range(30):
         _start = time()
         q = _get("/get_query").json()
         ans = {"winner": random.choice([q["left"], q["right"]]), "puid": puid, **q}
@@ -116,16 +116,40 @@ def test_basic():
     assert expected_cols == set(df.columns)
     assert df.puid.nunique() == 1
 
+
 def test_bad_file_upload():
     username, password = _get_auth()
     print(username, password)
     _get("/init_exp")
     exp = Path(__file__).parent / "data" / "bad_exp.yaml"
     r = _post(
-        "/init_exp", data={"exp": exp.read_bytes()}, auth=(username, password),
+        "/init_exp",
+        data={"exp": exp.read_bytes()},
+        auth=(username, password),
         error=True,
     )
     assert r.status_code == 500
     assert "Error!" in r.text
     assert "yaml" in r.text
     assert "-\tfoo" in r.text
+
+
+def test_no_repeats():
+    username, password = _get_auth()
+    exp = Path(__file__).parent / "data" / "exp.yaml"
+    _post(
+        "/init_exp", data={"exp": exp.read_bytes()}, auth=(username, password),
+    )
+    for k in range(100):
+        q = _get("/get_query").json()
+        ans = {"winner": random.choice([q["left"], q["right"]]), "puid": "foo", **q}
+        _post("/process_answer", data=ans)
+
+    r = _get("/get_responses", auth=(username, password))
+    df = pd.DataFrame(r.json())
+    equal_targets = (
+        (df["head"] == df["right"]).any()
+        or (df["head"] == df["left"]).any()
+        or (df["left"] == df["right"]).any()
+    )
+    assert not equal_targets
