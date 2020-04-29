657a49ac9ab15c052cd7d9c99525a02b429eb599
Scott <stsievert@users.noreply.github.com>
2020-04-29 16:32:15 -0500

    & for gunicorn

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@ then
     sleep 1
     uvicorn salmon:frontend --reload --port 8421 --host 0.0.0.0
 else
-    gunicorn -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8400 salmon:backend
+    gunicorn -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8400 salmon:backend &
     sleep 1
     gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8421 salmon:frontend
 fi
