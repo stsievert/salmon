660618b95a93cfe42ec23e5ac028db5b3e3515fe
Scott <stsievert@users.noreply.github.com>
2020-04-30 09:55:32 -0500

    Add version

--- salmon/__init__.py
+++ salmon/__init__.py
@@ -1,2 +1,8 @@
 from .frontend import app as frontend
 from .backend import app as backend
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+frontend.version = backend.version = __version__
--- salmon/frontend/__init__.py
+++ salmon/frontend/__init__.py
@@ -1,3 +1,2 @@
 from .private import *
 from .public import app
-__version__ = app.version = "v0.3.0"
