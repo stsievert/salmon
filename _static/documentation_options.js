bbdcb3d82ec43590dd2b37060d5ce2a277996ae6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-09-16 17:05:57 +0000

    Update docs after building Travis build 662 of stsievert/salmon
    The docs were built from the branch 'master' against the commit
    18180756acd818ad4574a9e7e3f7aadc4528e901.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/stsievert/salmon/jobs/385859918.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --exclude salmon/frontend/static --built-docs docs/build/html/ .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.4.0+5.gff8c78a.dirty',
+    VERSION: 'v0.4.0+6.g1818075.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
