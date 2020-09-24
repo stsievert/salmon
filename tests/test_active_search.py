7048e26178cb05a15c2118f8b9b5e517d0758b98
Scott <stsievert@users.noreply.github.com>
2020-09-24 10:22:50 -0500

    TST: better test

--- tests/test_active_search.py
+++ tests/test_active_search.py
@@ -23,14 +23,11 @@ def answer(X, y):
         for (h, l, r), yi in zip(X, y)
     ]
     alg_ans = [
-            (
-                a["head"],
-                a["winner"],
-                a["left"] if a["winner"] == a["right"] else a["right"]
-            )
-            for a in answers
+        (a["head"], a["winner"], a["left"] if a["winner"] == a["right"] else a["right"])
+        for a in answers
     ]
     return alg_ans
+
 
 def test_same_salmon_next(n=40, d=2, num_ans=4000):
     X, y = dataset(n, num_ans=num_ans, random_state=42)
@@ -43,12 +40,12 @@ def test_same_salmon_next(n=40, d=2, num_ans=4000):
         new_embedding[s1] = s2
         new_embedding[s2] = s1
 
-    search = InfoGainScorer(random_state=42, embedding=new_embedding, probs=est.opt.module_.probs)
+    search = InfoGainScorer(
+        random_state=42, embedding=new_embedding, probs=est.opt.module_.probs
+    )
     search.push(ans)
 
-    _, (useful, useless) = search.score(queries=[
-        (29, 30, 10), (29, 11, 28)
-    ])
+    _, (useful, useless) = search.score(queries=[(29, 30, 10), (29, 11, 28)])
     assert useful > useless
 
     t = list(range(40))
@@ -59,9 +56,14 @@ def test_same_salmon_next(n=40, d=2, num_ans=4000):
     ]
     Q, scores = search.score(queries=queries)
     flipped = [30, 10]
-    useful = np.isin(Q[:, 0], flipped) | np.isin(Q[:, 1], flipped) | np.isin(Q[:, 2], flipped)
+    useful = (
+        np.isin(Q[:, 0], flipped)
+        | np.isin(Q[:, 1], flipped)
+        | np.isin(Q[:, 2], flipped)
+    )
     useful = np.isin(Q[:, 1], flipped) & np.isin(Q[:, 2], flipped)
-    useful &= np.isin(Q[:, 0], [29, 31, 9, 11])
+    side_info = [29, 31, 9, 11]
+    useful &= np.isin(Q[:, 0], side_info)
     assert scores[useful].mean() > scores[~useful].mean()
     assert np.median(scores[useful]) > np.median(scores[~useful])
 
@@ -70,3 +72,24 @@ def test_same_salmon_next(n=40, d=2, num_ans=4000):
     assert np.percentile(scores[~useful], 70) < np.percentile(scores[useful], 30)
     assert np.percentile(scores[~useful], 80) < np.percentile(scores[useful], 50)
     assert np.percentile(scores[~useful], 90) < np.percentile(scores[useful], 75)
+
+    # Another test to make sure that "useless" queries have lower scores than
+    # "useful" queries.
+    all_queries = set(range(40))
+    good_items = sum([list(range(q - 4, q + 4 + 1)) for q in side_info + flipped], [])
+    useless = np.isin(Q[:, 0], list(all_queries - set(good_items)))
+    useless |= np.isin(Q[:, 1], list(all_queries - set(good_items)))
+    useless |= np.isin(Q[:, 2], list(all_queries - set(good_items)))
+
+    assert -1.1 < np.median(scores[useless]) + 0.3 < np.median(scores[useful]) < -0.75
+    assert -1.13 < scores[useless].mean() + 0.19 < scores[useful].mean() < -0.93
+    assert -1.85 < scores[useless].min() < scores[useful].min() < -1.46
+
+    p = np.percentile
+    assert -1.29 < p(scores[useless], 30) < p(scores[useful], 19) < -1.23
+    assert -1.17 < p(scores[useless], 40) < p(scores[useful], 23) < 1.04
+    assert -1.10 < p(scores[useless], 50) < p(scores[useful], 24) < -0.98
+    assert -1.05 < p(scores[useless], 60) < p(scores[useful], 25) < -0.94
+    assert -0.93 < p(scores[useless], 70) < p(scores[useful], 26) < -0.90
+    assert -0.80 < p(scores[useless], 85) < p(scores[useful], 30) < -0.77
+    assert -0.76 < p(scores[useless], 90) < p(scores[useful], 80) < -0.74
