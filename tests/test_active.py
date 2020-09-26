stsievert/salmon

commit 47ec3012432d90723698a092c2df8e8662ae1cfa

MAINT: Clean adaptive algorithms (#71)
* API: adds endpoint for alg timings
* VIZ: Visualizes algorithm timing on the dashboard.
* BUG: fixes error with epsilon.
* MAINT: flips the queries randomly for users.
* API: allows targets to be an integer (to create N targets)
* DOC: Adds dashboard to documentation.

examples/num-users/sim.py
@@ -1,0 +1,303 @@
+"""
+This script runs a simulation with the Salmon server launched at SALMON.
+
+It does the following:
+
+* Uses the strange fruit dataset
+* Pings the API to get query, answers, then sends the answer
+* Every $n$ targets, it gets the embedding and computed the accuracy/etc.
+
+Stats to record include accuracy and distances to nearest neighbor (median, mean, etc).
+"""
+
+import asyncio
+import json
+import sys
+from pathlib import Path
+from time import time
+from typing import Optional, Tuple, Union
+
+import httpx
+import numpy as np
+import msgpack
+import pandas as pd
+import yaml
+from scipy.spatial import distance_matrix
+from sklearn.base import BaseEstimator
+from sklearn.utils import check_random_state
+
+from salmon.triplets.algs import TSTE
+
+DIR = Path(__file__).absolute().parent
+sys.path.append(str(DIR.parent))
+import datasets
+
+sys.path.append(str(DIR.parent / "queries-searched"))
+from run import _test_dataset
+
+SALMON = "http://localhost:8421"
+
+
+class SalmonExperiment(BaseEstimator):
+    def __init__(
+        self, salmon=SALMON, dataset="strange_fruit", n=200, d=2, R=10, random_state=None, init=True,
+    ):
+        self.salmon = salmon
+        self.dataset = dataset
+        self.n = n
+        self.d = d
+        self.R = R
+        self.random_state = random_state
+        self.init = init
+
+    def initialize(self):
+        self.random_state_ = check_random_state(self.random_state)
+        if self.init:
+            httpx.get(self.salmon + "/reset?force=1", auth=("username", "password"), timeout=20)
+        if self.dataset == "strange_fruit":
+            init = {
+                "d": self.d,
+                "samplers": {"TSTE": {"alpha": 1, "random_state": self.random_state, "R": self.R}},
+                "targets": list(range(self.n)),
+            }
+            if not self.init:
+                self.config = init
+                return self
+            httpx.post(
+                self.salmon + "/init_exp",
+                data={"exp": yaml.dump(init)},
+                auth=("username", "password"),
+            )
+        else:
+            raise ValueError(f"dataset={self.dataset} not recognized")
+
+        self.config = init
+        return self
+
+
+class User(BaseEstimator):
+    def __init__(
+        self,
+        salmon=SALMON,
+        n_responses=100,
+        response_time=1,
+        reaction_time=0.75,
+        random_state=None,
+        http=None,
+        uid="",
+    ):
+        self.salmon = salmon
+        self.response_time = response_time
+        self.n_responses = n_responses
+        self.random_state = random_state
+        self.reaction_time = reaction_time
+        self.http = http
+        self.uid = uid
+
+    def init(self):
+        self.initialized_ = True
+        self.random_state_ = check_random_state(self.random_state)
+        self.data_ = []
+        return self
+
+    async def _partial_fit(self, X=None, y=None):
+        if not hasattr(self, "initialized_") or not self.initialized_:
+            self.init()
+
+        for k in range(self.n_responses):
+            try:
+                datum = {"num_responses": k, "puid": self.uid, "salmon": self.salmon}
+                sleep_time = self.random_state_.uniform(0, 5)
+                await asyncio.sleep(sleep_time)
+
+                _s = time()
+                r = await self.http.get(self.salmon + "/query", timeout=20)
+                datum.update({"time_get_query": time() - _s})
+                assert r.status_code == 200
+
+                query = r.json()
+                _ans = datasets.strange_fruit(
+                    query["head"],
+                    query["left"],
+                    query["right"],
+                    random_state=self.random_state_,
+                )
+                winner = query["left"] if _ans == 0 else query["right"]
+                sleep_time = self.random_state_.normal(loc=self.response_time, scale=0.25)
+                sleep_time = max(self.reaction_time, sleep_time)
+                answer = {
+                    "winner": winner,
+                    "puid": self.uid,
+                    "response_time": sleep_time,
+                    **query,
+                }
+                if self.uid == "0":
+                    h = answer["head"]
+                    l = answer["left"]
+                    r = answer["right"]
+                    w = answer["winner"]
+                    dl = abs(h - l)
+                    dr = abs(h - r)
+                    if w == l:
+                        print(f"DL={dl}, dr={dr}. (h, l, r, w) = {(h, l, r, w)}")
+                    elif w == r:
+                        print(f"dl={dl}, DR={dr}. (h, l, r, w) = {(h, l, r, w)}")
+                    else:
+                        raise ValueError(f"h, l, r, w = {(h, l, r, w)}")
+                await asyncio.sleep(sleep_time)
+                datum.update({"sleep_time": sleep_time})
+                _s = time()
+                r = await self.http.post(self.salmon + "/answer", data=json.dumps(answer), timeout=20)
+                datum.update({"time_post_answer": time() - _s})
+                assert r.status_code == 200
+                self.data_.append(datum)
+            except Exception as e:
+                print("Exception!")
+                print(e)
+        return self
+
+    def partial_fit(self, X=None, y=None):
+        return self._partial_fit(X=X, y=y)
+
+
+class Stats:
+    def __init__(
+        self,
+        X,
+        y,
+        *,
+        sampler,
+        config=None,
+        fname="stats.parquet",
+        http=None,
+        salmon=SALMON,
+    ):
+        self.sampler = sampler
+        self.salmon = salmon
+        self.http = http
+        self.fname = fname
+        self.history_ = []
+        self.config = config
+        self.X = X
+        self.y = y
+
+    def _fmt_responses(self, responses):
+        return [
+            [
+                r["head"],
+                r["winner"],
+                r["left"] if r["winner"] == r["right"] else r["right"],
+                r["score"],
+            ]
+            for r in responses
+        ]
+
+    async def collect(self):
+        response = await self.http.get(self.salmon + f"/model/{self.sampler}")
+        stats = response.json()
+        stats["time"] = time()
+        if "embedding" not in stats.keys():
+            print("Embedding not in keys!")
+            print(stats)
+            return {"error": True}
+        embedding = np.asarray(stats.pop("embedding"))
+        accuracy = self._get_acc(embedding)
+        nn_acc, nn_diffs = self._get_nn_diffs(embedding)
+
+        diff_stats = {
+            f"nn_diff_p{k}": np.percentile(nn_diffs, k)
+            for k in [99, 95, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 1]
+        }
+
+        r = await self.http.get(
+            self.salmon + f"/responses", auth=("username", "password")
+        )
+        responses = self._fmt_responses(r.json())
+
+        return {
+            "accuracy": accuracy,
+            "nn_diff_median": np.median(nn_diffs),
+            "nn_diff_mean": nn_diffs.mean(),
+            "nn_acc": nn_acc,
+            "responses": responses,
+            "error": False,
+            **diff_stats,
+            **stats,
+        }
+
+    def _get_acc(self, embedding) -> float:
+        alg = TSTE(self.config["n"], self.config["d"])
+        y_hat = alg.predict(self.X, embedding=embedding)
+        acc = (self.y == y_hat).mean()
+        return acc
+
+    def _get_nn_diffs(self, embedding) -> Tuple[float, np.ndarray]:
+        dists = distance_matrix(embedding, embedding)
+        dists[dists <= 0] = np.inf
+        neighbors = dists.argmin(axis=1)
+        neighbor_dists = np.abs(neighbors - np.arange(len(neighbors)))
+        nn_acc = (neighbor_dists == 1).mean()
+        return nn_acc, neighbor_dists
+
+    async def run_until(self, event):
+        while True:
+            deadline = time() + 5
+            datum = await self.collect()
+            self.history_.append(datum)
+            fname = self.fname.format(n_users=self.config["n_users"])
+            with open(fname, "wb") as f:
+                msgpack.dump(self.history_, f)
+            if event.is_set():
+                return self.history_, fname
+            while time() < deadline:
+                await asyncio.sleep(0.1)
+
+
+async def main(**config):
+    kwargs = {k: config[k] for k in ["random_state", "dataset", "n", "d", "init"]}
+    exp = SalmonExperiment(**kwargs).initialize()
+
+    X, y = _test_dataset(config["n"], config["n_test"])
+    n_responses = (config["max_queries"] // config["n_users"]) + 1
+
+    kwargs = {k: config[k] for k in ["reaction_time", "response_time"]}
+    async with httpx.AsyncClient() as client:
+        users = [
+            User(http=client, random_state=i, uid=str(i), n_responses=n_responses, **kwargs)
+            for i in range(config["n_users"])
+        ]
+        responses = [user.partial_fit() for user in users]
+        completed = asyncio.Event()
+        algs = list(exp.config["samplers"].keys())
+        assert len(algs) == 1
+        stats = Stats(
+            X, y, config=config, sampler=algs[0], http=client, fname=config["fname"]
+        )
+        task = asyncio.create_task(stats.run_until(completed))
+        await asyncio.gather(*responses)
+        user_data = sum([user.data_ for user in users], [])
+        completed.set()
+        while not task.done():
+            await asyncio.sleep(0.1)
+        history, fname = task.result()
+    return history, fname, user_data
+
+
+if __name__ == "__main__":
+    config = {
+        "n_users": 10,
+        "max_queries": 2000,
+        "n": 50,
+        "d": 1,
+        "R": 10,
+        "dataset": "strange_fruit",
+        "random_state": 42,
+        "n_test": 10_000,
+        "fname": "history-n_users={n_users}.msgpack",
+        "reaction_time": 0,
+        "response_time": 0.1,
+        "init": False,
+    }
+    history, fname, user_data = asyncio.run(main(**config))
+    with open(f"user-{fname}.json", "w") as f:
+        json.dump(history, f)

salmon/backend/alg.py
@@ -16,6 +16,7 @@ logger = get_logger(__name__)
 
 Query = TypeVar("Query")
 Answer = TypeVar("Answer")
+root = Path.rootPath()
 
 
 class Runner:
@@ -67,8 +68,11 @@ class Runner:
         queries = np.array([])
         scores = np.array([])
         n_model_updates = 0
+        rj.jsonset(f"alg-perf-{self.ident}", root, [])
+        save_deadline = 0.0  # right away
         for k in itertools.count():
             try:
+                loop_start = time()
                 datum = {"iteration": k, "ident": self.ident}
 
                 answers = self.get_answers(rj, clear=True)
@@ -79,6 +83,7 @@ class Runner:
                 queries_f = client.scatter(queries)
                 scores_f = client.scatter(scores)
                 if update:
+                    datum["cleared_queries"] = True
                     self.clear_queries(rj)
                 done = distributed.Event(name="pa_finished")
                 done.clear()
@@ -141,8 +146,17 @@ class Runner:
                 logger.exception(e)
 
             _s = time()
-            self.save()
+            if time() >= save_deadline:
+                minute = 60
+                save_deadline = time() + 2 * minute
+                self.save()
             datum["time_save"] = time() - _s
+            datum["time_loop"] = time() - loop_start
+            rj.jsonarrappend(f"alg-perf-{self.ident}", root, datum)
+            logger.info(datum)
+            from pprint import pprint
+
+            pprint(datum)
             if "reset" in rj.keys() and rj.jsonget("reset"):
                 self.reset(client, rj)
                 break
@@ -150,11 +164,9 @@ class Runner:
             pprint(datum)
         return True
 
-    def min_search_length(self):
-        return 1
-
     def save(self) -> bool:
         rj2 = self.redis_client(decode_responses=False)
+
         out = cloudpickle.dumps(self)
         rj2.set(f"state-{self.ident}", out)
 

commit f523620dc3811931a3ceb64a8645edf934860a1e

MAINT: prepare for road test of adaptive algorithms (#76)
* API: allow sampling algorithms with different frequency
* MAINT: allow upload of ZIP files again (accidentally broken in #71).
* MAINT, TST: better error handling of wrong parameters.
* VIZ: adds visualization of response rate

.gitignore
@@ -9,6 +9,7 @@ out/*.csv
 out/*.log
 *.DS_Store
 *.swp
+salmon/frontend/static/targets/*
 
 # Byte-compiled / optimized / DLL files
 __pycache__/

examples/num-users/sim.py
@@ -285,7 +285,7 @@ async def main(**config):
 
 if __name__ == "__main__":
     config = {
-        "n_users": 10,
+        "n_users": 20,
         "max_queries": 2000,
         "n": 50,
         "d": 1,
@@ -294,9 +294,9 @@ if __name__ == "__main__":
         "random_state": 42,
         "n_test": 10_000,
         "fname": "history-n_users={n_users}.msgpack",
-        "reaction_time": 0,
+        "reaction_time": 0.1,
         "response_time": 0.1,
-        "init": False,
+        "init": True,
     }
     history, fname, user_data = asyncio.run(main(**config))
     with open(f"user-{fname}.json", "w") as f:

examples/strange-fruit-triplet/init.yaml
@@ -1,0 +1,15 @@
+instructions: Please select the comparison that is most similar to the target.
+debrief: Thanks!
+# Time: took 1:10 for 50 queries
+max_queries: 50
+sampling:
+    probs: {"TSTE": 40, "random": 40, "test": 20}
+samplers:
+  TSTE:
+    random_state: 42
+  random:
+    module: RandomSampling
+    random_state: 10201
+  test:
+    module: RandomSampling
+    random_state: 1

salmon/backend/alg.py
@@ -80,8 +80,11 @@ class Runner:
                 self_future = client.scatter(self)
 
                 _start = time()
-                queries_f = client.scatter(queries)
-                scores_f = client.scatter(scores)
+                if len(queries) and len(scores):
+                    queries_f = client.scatter(queries)
+                    scores_f = client.scatter(scores)
+                else:
+                    queries_f = scores_f = []
                 if update:
                     datum["cleared_queries"] = True
                     self.clear_queries(rj)
@@ -97,7 +100,7 @@ class Runner:
                         "get_queries", self_future, random_state=k, stop=done
                     )
                 else:
-                    f_search = client.submit(lambda x: x, 1)
+                    f_search = client.submit(lambda x: ([], []), 0)
 
                 time_model = 0
                 time_post = 0

salmon/frontend/plotting.py
@@ -294,6 +294,7 @@ async def _get_alg_perf(df):
         x_axis_type="datetime",
         width=600,
         height=300,
+        toolbar_location="above",
     )
     names = list(reversed(cols))
     p.varea_stack(
@@ -304,4 +305,35 @@ async def _get_alg_perf(df):
         source=source,
     )
     p.legend.location = "top_left"
+    return p
+
+async def response_rate(df, n_sec=30):
+    df = df.copy()
+    df["time_since_start"] = df["time_received"] - df["time_received"].min()
+    df["response_received"] = 1
+
+    s = df[["response_received", "time_since_start"]].copy()
+    s["time_since_start"] = s.time_since_start.apply(lambda x: timedelta(seconds=x))
+    base = s.copy()
+
+    for _sec in [1e-3, n_sec + 1e-3]:
+        t = base.copy()
+        t["time_since_start"] += timedelta(seconds=_sec)
+        t["response_received"] = 0
+        s = pd.concat((t, s))
+    s = s.sort_values(by="time_since_start").set_index("time_since_start")
+
+    ss = s.rolling(window=f"{n_sec}s").sum() / n_sec
+    source = ColumnDataSource(ss)
+
+    p = figure(
+        title="Responses per second",
+        x_axis_type="datetime",
+        x_axis_label="Time since start",
+        y_axis_label="(30s moving avg)",
+        width=600,
+        height=200,
+        toolbar_location="above",
+    )
+    p.varea(x="time_since_start", y1="response_received", source=source)
     return p

salmon/frontend/private.py
@@ -31,6 +31,8 @@ from rejson import Client, Path
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.requests import Request
 from starlette.status import HTTP_401_UNAUTHORIZED
+from redis import ResponseError
+
 
 import salmon
 from ..triplets import manager
@@ -151,8 +153,6 @@ def upload_form():
 
 async def _get_config(exp: bytes, targets: bytes) -> Dict[str, Any]:
     config = yaml.load(exp, Loader=yaml.SafeLoader)
-    if isinstance(config["targets"], int):
-        config["targets"] = [str(x) for x in range(config["targets"])]
     exp_config: Dict = {
         "instructions": "Default instructions (can include <i>arbitrary</i> HTML)",
         "max_queries": None,
@@ -163,6 +163,29 @@ async def _get_config(exp: bytes, targets: bytes) -> Dict[str, Any]:
         "skip_button": False,
     }
     exp_config.update(config)
+    if "sampling" not in exp_config:
+        n = len(exp_config["samplers"])
+        freqs = [100 // n] * n
+        freqs[0] += 100 % n
+        sampling_percent = {k: f for k, f in zip(exp_config["samplers"], freqs)}
+        exp_config["sampling"] = {"probs": sampling_percent}
+
+    if set(exp_config["sampling"]["probs"]) != set(exp_config["samplers"]):
+        sf = set(exp_config["sampling"]["probs"])
+        s = set(exp_config["samplers"])
+        msg = (
+            "sampling.probs keys={} are not the same as samplers keys={}.\n\n"
+            "Keys in sampling.probs but not in samplers: {}\n"
+            "Keys in samplers but but in sampling.probs: {}\n\n"
+        )
+        raise ValueError(msg.format(sf, s, sf - s, s - sf))
+    if sum(exp_config["sampling"]["probs"].values()) != 100:
+        msg = (
+            "The values in sampling.probs should add up to 100; however, "
+            "the passed sampling.probs={} adds up to {}"
+        )
+        s = exp_config["sampling"]["probs"]
+        raise ValueError(msg.format(s, sum(s.values())))
 
     if targets:
         fnames = _extract_zipfile(targets)
@@ -172,6 +195,8 @@ async def _get_config(exp: bytes, targets: bytes) -> Dict[str, Any]:
         else:
             targets = [_format_target(f) for f in fnames]
             exp_config["targets"] = targets
+    elif isinstance(config["targets"], int):
+        exp_config["targets"] = [str(x) for x in range(config["targets"])]
     else:
         exp_config["targets"] = [str(x) for x in exp_config["targets"]]
 
@@ -181,13 +206,13 @@ async def _get_config(exp: bytes, targets: bytes) -> Dict[str, Any]:
 
 
 def exception_to_string(excp):
-    stack = traceback.extract_stack() + traceback.extract_tb(
-        excp.__traceback__
-    )  # add limit=??
+    stack = traceback.extract_stack() + traceback.extract_tb(excp.__traceback__)
     pretty = traceback.format_list(stack)
-    return "Error!\n\n\nSummary:\n\n{} {}\n\nFull traceback:\n\n".format(
-        excp.__class__, excp
-    ) + "".join(pretty)
+    return "Error:\n\n{}\n\nMessage:\n\n{}\n\n\nSummary:\n\n{} {}\n\nTraceback:\n\n".format(
+        str(excp), getattr(excp, "detail", ""), excp.__class__, excp
+    ) + "".join(
+        pretty
+    )
 
 
 class ExpParsingError(StarletteHTTPException):
@@ -229,8 +254,11 @@ async def process_form(
         - max_queries: 25
     """
     try:
-        return await _process_form(request, exp, targets, rdb)
+        ret = await _process_form(request, exp, targets, rdb)
+        await _ensure_initialized()
+        return ret
     except Exception as e:
+        logger.error(e)
         reset(force=True, timeout=2)
         if isinstance(e, ExpParsingError):
             raise e
@@ -254,7 +282,10 @@ async def _process_form(
 
     # Start the backend
     names = list(exp_config["samplers"].keys())
+    _probs = exp_config["sampling"]["probs"]
+    probs = [_probs[n] / 100 for n in names]
     rj.jsonset("samplers", root, names)
+    rj.jsonset("sampling_probs", root, probs)
     for name in names:
         rj.jsonset(f"alg-{name}-answers", root, [])
 
@@ -315,10 +346,13 @@ def reset(
         raise ServerException(msg)
 
     if authorized:
-        logger.error(
-            "Resetting, force=True and authorized. Removing data from database"
-        )
-        rj.save()
+        logger.error("Authorized reset, force=True. Removing data from database")
+        try:
+            rj.save()
+        except ResponseError as e:
+            if "save already in progress" not in str(e):
+                raise e
+
         now = datetime.now().isoformat()[: 10 + 6]
 
         save_dir = ROOT_DIR / "out"
@@ -466,10 +500,12 @@ async def get_dashboard(request: Request, authorized: bool = Depends(_authorize)
         activity = await plotting.activity(df, start)
         response_times = await plotting.response_time(df)
         network_latency = await plotting.network_latency(df)
+        response_rate = await plotting.response_rate(df)
         plots = {
             "activity": activity,
             "response_times": response_times,
             "network_latency": network_latency,
+            "response_rate": response_rate,
         }
         plots = {k: json.dumps(json_item(v)) for k, v in plots.items()}
     except Exception as e:
@@ -499,18 +535,20 @@ async def get_dashboard(request: Request, authorized: bool = Depends(_authorize)
         endpoints[1], endpoints[i] = endpoints[i], endpoints[1]
 
     idents = rj.jsonget("samplers")
-    try:
-        models = {alg_ident: await get_model(alg_ident) for alg_ident in idents}
-        alg_plots = {
-            alg: await plotting.show_embedding(model["embedding"], targets, alg=alg)
-            for alg, model in models.items()
-        }
-        alg_plots = {k: json.dumps(json_item(v)) for k, v in alg_plots.items()}
-        logger.info(f"idents = {idents}")
-    except Exception as e:
-        logger.exception(e)
-        alg_plots = {"/": "exception"}
-        models = {"/": ""}
+    models = {}
+    for alg in idents:
+        try:
+            models[alg] = await get_model(alg)
+        except Exception as e:
+            models[alg] = None
+    alg_plots = {
+        alg: await plotting.show_embedding(model["embedding"], targets, alg=alg)
+        for alg, model in models.items() if model
+    }
+    alg_plots = {k: json.dumps(json_item(v)) for k, v in alg_plots.items() if v}
+    if not len(alg_plots):
+        alg_plots = {"no embeddings": None}
+    logger.info(f"idents = {idents}")
 
     try:
         perfs = {ident: await _get_alg_perf(ident) for ident in idents}
@@ -648,8 +686,7 @@ async def get_model(alg_ident: str) -> Dict[str, Any]:
     logger.info("In public get_model with rj.keys() == %s", rj.keys())
     r = httpx.get(f"http://localhost:8400/model/{alg_ident}")
     if r.status_code != 200:
-        msg = r.json()["detail"]
-        raise ServerException(msg)
+        raise ServerException(r.json()["detail"])
     return r.json()
 
 
@@ -657,8 +694,7 @@ async def _get_alg_perf(ident: str) -> Dict[str, Any]:
     logger.info("In private _get_alg_perf with rj.keys() == %s", rj.keys())
     r = httpx.get(f"http://localhost:8400/meta/perf/{ident}")
     if r.status_code != 200:
-        msg = r.json()["detail"]
-        raise ServerException(msg)
+        raise ServerException(r.json()["detail"])
     return r.json()
 
 

salmon/frontend/public.py
@@ -83,6 +83,7 @@ async def _ensure_initialized():
         "max_queries",
         "debrief",
         "skip_button",
+        "sampling",
     ]
     if not set(exp_config) == set(expected_keys):
         msg = (
@@ -92,8 +93,9 @@ async def _ensure_initialized():
         )
         extra = set(exp_config) - set(expected_keys)
         missing = set(expected_keys) - set(exp_config)
-        out = msg.format(expected_keys, list(exp_config.keys()), extra, missing)
-        raise ServerException()
+        raise ServerException(
+            msg.format(expected_keys, list(exp_config.keys()), extra, missing)
+        )
     return exp_config
 
 
@@ -120,7 +122,9 @@ async def get_query_page(request: Request):
 @app.get("/query", tags=["public"])
 async def get_query() -> Dict[str, Union[int, str, float]]:
     idents = rj.jsonget("samplers")
-    ident = random.choice(idents)
+    probs = rj.jsonget("sampling_probs")
+
+    ident = np.random.choice(idents, p=probs)
 
     r = httpx.get(f"http://localhost:8400/query-{ident}")
     logger.info(f"query r={r}")

salmon/frontend/utils.py
@@ -16,6 +16,7 @@ logger = get_logger(__name__)
 
 class ServerException(HTTPException):
     def __init__(self, msg):
+        logger.error(msg)
         raise HTTPException(status_code=500, detail=msg)
 
 

templates/dashboard.html
@@ -42,6 +42,13 @@
       Bokeh.embed.embed_item(plot, "activity-plot");
       </script>
     </div>
+    <div class="row justify-content-center">
+      <div id="concurrent-users"></div>
+      <script>
+      plot = JSON.parse({{ response_rate | tojson }});
+      Bokeh.embed.embed_item(plot, "concurrent-users");
+      </script>
+    </div>
     <br>
     <div class="row justify-content-center">
       <h3>Basic information</h3>
@@ -138,7 +145,7 @@
               <a class="nav-link {% if loop.first %}active{% endif %}"
                  id="{{endpoint}}-tab"
                  data-toggle="tab"
-                 href="#{{alg}}"
+                 href="#{{alg}}-perf"
                  role="tab"
                  aria-controls="{{endpoint}}"
                  aria-selected={% if loop.first %}"true"{% else %}"false"{% endif %}

templates/query_page.html
@@ -151,6 +151,7 @@ async function send(winner, id) {
   $('#q-left').addClass("disabled");
   $('#q-right').addClass("disabled");
   $("#num-queries").html("" + num_queries);
+  console.log(ident);
   $.post("/answer",
     JSON.stringify({
         "head": head,

tests/test_active.py
@@ -17,6 +17,36 @@ from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 from .utils import server, logs
 
 
+def test_active_wrong_proportion(server, logs):
+    server.authorize()
+    exp = {
+        "targets": 10,
+        "sampling": {"probs": {"a1": 50, "a2": 40}},
+        "samplers": {
+            "a1": {"module": "RandomSampling"},
+            "a2": {"module": "RandomSampling"},
+        },
+    }
+    r = server.post("/init_exp", data={"exp": json.dumps(exp)}, error=True)
+    assert r.status_code == 500
+    assert "values in sampling.probs should add up to 100" in r.text
+
+
+def test_active_bad_keys(server, logs):
+    server.authorize()
+    exp = {
+        "targets": 10,
+        "sampling": {"probs": {"a1": 50, "a2": 40}},
+        "samplers": {"a1": {"module": "RandomSampling"}},
+    }
+    r = server.post("/init_exp", data={"exp": json.dumps(exp)}, error=True)
+    assert r.status_code == 500
+    assert all(
+        x in r.text.lower()
+        for x in ["sampling.probs keys", "are not the same as samplers keys"]
+    )
+
+
 def test_active_basics(server, logs):
     server.authorize()
     exp = Path(__file__).parent / "data" / "active.yaml"
@@ -86,4 +116,3 @@ def test_round_robin(server, logs):
     #    File "/opt/conda/lib/python3.7/json/decoder.py", line 337, in decode
     #      obj, end = self.raw_decode(s, idx=_w(s, 0).end())
     #  TypeError: expected string or bytes-like object
-

tests/test_basic.py
@@ -108,7 +108,7 @@ def test_bad_file_upload(server):
     exp = Path(__file__).parent / "data" / "bad_exp.yaml"
     r = server.post("/init_exp", data={"exp": exp.read_bytes()}, error=True)
     assert r.status_code == 500
-    assert "Error!" in r.text
+    assert "Error" in r.text
     assert "yaml" in r.text
     assert "-\tfoo" in r.text
 

tests/utils.py
@@ -13,6 +13,7 @@ from sklearn.utils import check_random_state
 
 logger = getLogger(__name__)
 
+
 class Server:
     def __init__(self, url, async_client=None):
         self.url = url
@@ -72,6 +73,7 @@ def _clear_logs(log=None):
         for log in log_dir.glob("*.log"):
             log.write_text("")
 
+
 @pytest.fixture()
 def server():
     server = Server("http://127.0.0.1:8421")
@@ -87,8 +89,10 @@ def server():
     if dump.exists():
         dump.unlink()
 
+
 class LogError(Exception):
     pass
+
 
 class Logs:
     def __init__(self):
@@ -114,6 +118,7 @@ class Logs:
                 if self.warn and "warn" in line:
                     warn("{}\n{}".format(log, line))
         _clear_logs()
+
 
 @pytest.fixture()
 def logs():