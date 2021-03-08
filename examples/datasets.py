1c2b758050590bcda4679880ee5772724fb61887
Scott <stsievert@users.noreply.github.com>
2021-03-08 17:05:52 -0600

    New noise model for alien eggs with n=30

--- examples/datasets.py
+++ examples/datasets.py
@@ -5,30 +5,34 @@ import pandas as pd
 from sklearn.utils import check_random_state
 
 
-def alien_egg(head, left, right, random_state=None):
+def _sigmoid(x, err, rate):
+    noiseless = 1 / (1 + np.exp(-rate * (x - 0.5)))
+    p_correct = (1 - err * 2) * (noiseless - 0.5) + 0.5
+    return p_correct
+
+
+def alien_egg(h, l, r, random_state=None, err=0.07074231, rate=17.55473809) -> int:
     """
     Parameters
     ----------
-    head, left, right : int, int, int
+    h, l, r : int, int, int
         Number of spikes on the various objects
 
     Returns
     -------
-    winner : str
-        Either "left" or "right"
+    winner : int
+        0 if left wins, 1 if right wins
 
     Notes
     -----
     This is determined from human data.
-    See datasets/strange-fruit-triplet/noise-model.ipynb for details.
+    See datasets/alien-egg/alien-eggs-noise-model-n=30/Noise-model.ipynb for details.
     """
-    ldiff = np.abs(head - left)
-    rdiff = np.abs(head - right)
+    ldiff = np.abs(h - l)
+    rdiff = np.abs(h - r)
 
     r = np.maximum(ldiff, rdiff) / (ldiff + rdiff)
-    rate = 19.5269746
-    final = 0.9567
-    p_correct = final / (1 + np.exp(-rate * (r - 0.5)))
+    p_correct = _sigmoid(r, err, rate)
 
     winner = 0 if ldiff < rdiff else 1
     random_state = check_random_state(random_state)
