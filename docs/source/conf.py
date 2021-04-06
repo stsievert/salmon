db279f20ecc0b10714f7c687caa55826b9aa1474
Scott <stsievert@users.noreply.github.com>
2021-04-06 12:36:29 -0500

    Remove '.dirty' from HTML title on docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = "Scott Sievert"
 # The full version, including alpha/beta/rc tags
 from salmon import __version__
 
-release = __version__
+release = __version__.replace(".dirty", "")
 
 html_context = {
     "display_github": True,  # Integrate GitHub
