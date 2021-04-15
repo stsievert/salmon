6a966b10c43ea195053eceaadb9946e6f51f9c86
Scott <stsievert@users.noreply.github.com>
2021-04-15 10:32:26 -0500

    don't needlessly test

--- tests/test_offline.py
+++ tests/test_offline.py
@@ -66,7 +66,7 @@ def test_offline_embedding():
     assert all(isinstance(h, dict) for h in model.history_)
     epochs = model.history_[-1]["num_grad_comps"] / len(X_train)
     assert max_epochs - 0.3 <= epochs <= max_epochs + 0.3
-    assert len(model.history_) in [max_epochs - 1, max_epochs]
+
 
 def test_offline_embedding_random_state():
     n, d = 85, 2
@@ -76,6 +76,10 @@ def test_offline_embedding_random_state():
     X = np.random.choice(n, size=(10_000, 3))
     X_train, X_test = train_test_split(X, random_state=0, test_size=0.2)
 
-    m1 = OfflineEmbedding(n=n, d=d, max_epochs=max_epochs, random_state=random_state).initialize(X_train)
-    m2 = OfflineEmbedding(n=n, d=d, max_epochs=max_epochs, random_state=random_state).initialize(X_train)
+    m1 = OfflineEmbedding(
+        n=n, d=d, max_epochs=max_epochs, random_state=random_state
+    ).initialize(X_train)
+    m2 = OfflineEmbedding(
+        n=n, d=d, max_epochs=max_epochs, random_state=random_state
+    ).initialize(X_train)
     assert np.allclose(m1.embedding_, m2.embedding_)
