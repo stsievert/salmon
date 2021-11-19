3aebd22d81bc5409610cb67af9b3d3c8a579042e
Scott <stsievert@users.noreply.github.com>
2021-11-18 22:31:30 -0600

    reorder

--- salmon/triplets/test_config.py
+++ salmon/triplets/test_config.py
@@ -13,24 +13,18 @@ def test_old_html_keys(key):
 
 @pytest.mark.parametrize("key", html_keys)
 def test_html_propogates(key):
-    c = Config()
-
     vals = {"max_queries": 50, "skip_button": True}
     v = vals.get(key, "foobar")
 
     user_config = {"html": {key: v}}
-
-    c = c.parse(user_config)
+    c = Config().parse(user_config)
 
     assert c.html.dict()[key] == v
 
 
 def test_defaults():
-    c = Config()
-
     user_config = {"targets": [0, 1, 2, 3]}
-
-    c = c.parse(user_config)
+    c = Config().parse(user_config)
 
     assert c.dict() == {
         "targets": ["0", "1", "2", "3"],
@@ -52,48 +46,52 @@ def test_defaults():
 
 
 def test_random_old_name_warns():
-    c = Config()
     user_config = {"samplers": {"RandomSampling": {}}}
+    c = Config()
     with pytest.raises(ValueError, match="renamed to `Random`"):
         c.parse(user_config)
 
 
 def test_wrong_probs():
-    c = Config()
     user_config = {
         "samplers": {"Random": {}, "ARR": {}},
         "sampling": {"probs": {"Random": 50, "ARR": 40}},
     }
+    c = Config()
     with pytest.raises(ValueError, match="sampling.probs should add up to 100"):
         c.parse(user_config)
 
 
 def test_wrong_keys():
-    c = Config()
     user_config = {
         "samplers": {"Random": {}, "ARR_v1": {}},
         "sampling": {"probs": {"Random": 50, "ARR_v2": 50}},
     }
+    c = Config()
     with pytest.raises(ValueError, match="Keys in sampling.probs but not in samplers"):
         c.parse(user_config)
 
 
-@pytest.mark.parametrize("v", [2, 3])
+@pytest.mark.parametrize("v", [0, 1, 2, 3])
 def test_bad_samplers_per_keys(v):
     c = Config()
     user_config = {
         "samplers": {"Random": {}, "ARR": {}},
         "sampling": {"samplers_per_user": v},
     }
-    with pytest.raises(NotImplementedError, match="Only samplers_per_user in {0, 1}"):
+    if v in [0, 1]:
         c.parse(user_config)
+    else:
+        with pytest.raises(
+            NotImplementedError, match="Only samplers_per_user in {0, 1}"
+        ):
+            c.parse(user_config)
 
 
-def test_d_in_common_params():
-    c = Config()
+def test_d_default_in_common_params():
     user_config = {
         "samplers": {"ARR": {}},
         "sampling": {"common": {"foo": "bar"}},
     }
-    c = c.parse(user_config)
-    assert "d" in c.sampling.common and c.sampling.common["d"] == 2
+    c = Config().parse(user_config)
+    assert c.sampling.common == {"d": 2, "foo": "bar"}
