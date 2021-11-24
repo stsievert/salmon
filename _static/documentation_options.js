de0b14a6e27e8d92c604b287b143ebde691aa61b
stsievert <stsievert@users.noreply.github.com>
2021-11-24 04:29:12 +0000

    deploy: 1e9a48d15fb747a1b8ba57ce98bc65c3007c5bbf

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.8.0+45.g79edfad',
+    VERSION: 'v0.8.0+46.g1e9a48d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
